867f3e84c044389b696dbf322f2ab0c22b397f7c
daniela-huerta <1930474@upv.edu.mx>
2021-07-25 23:02:45 -0500

    Intento de intefaces bonitas

--- desktop_app/src/main/java/com/TeamPro/App.java
+++ desktop_app/src/main/java/com/TeamPro/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,6 +24,8 @@ public class App extends Application {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/Login.fxml"));
         Scene scene = loader.load();
         primaryStage.setTitle("Sistema inventario");
+        primaryStage.getIcons().add(new Image(App.class.getResourceAsStream( "/teampro.png" )));
+        //primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
